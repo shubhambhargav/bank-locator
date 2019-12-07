523cc0286ad84c07d726be33b125490cbf49eb3b
Shubham Bhargav <shubham@getpostman.com>
2019-12-07 20:55:17 +0530

    Added support for postgres

--- banker/settings.py
+++ banker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configuring database for heroku
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
